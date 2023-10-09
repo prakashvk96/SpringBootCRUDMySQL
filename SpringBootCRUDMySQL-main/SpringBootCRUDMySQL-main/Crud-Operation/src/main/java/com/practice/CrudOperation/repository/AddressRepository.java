9d5840596536da5b808d91e966572cec765b41dd
Prakash Kumar <34058573+prakashvk96@users.noreply.github.com>
2023-10-10 00:23:45 +0530

    Delete SpringBootCRUDMySQL-main/SpringBootCRUDMySQL-main directory

--- SpringBootCRUDMySQL-main/SpringBootCRUDMySQL-main/Crud-Operation/src/main/java/com/practice/CrudOperation/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.practice.CrudOperation.repository;
-
-import com.practice.CrudOperation.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address,Long> {
-}
--- SpringBootCRUDMySQL-main/SpringBootCRUDMySQL-main/Crud-Operation/src/main/java/com/practice/CrudOperation/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.practice.CrudOperation.repository;
-
-import com.practice.CrudOperation.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
